06b4bab118da041e9d034292e5823c772802fb19
Asus <asus@mail.ru>
2023-02-10 08:31:21 +0300

    Добавлены разрешения для нашего веб-API + добавлен сериализатор, представления и конечные точки для пользователей.

--- snippets/views.py
+++ snippets/views.py
@@ -1,19 +1,43 @@
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
+from .permissions import IsOwnerReadOnly
 from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import permissions
 
 
-class Snippet_list(generics.ListCreateAPIView):
+class SnippetList(generics.ListCreateAPIView):
     """
     Выводит все фрагменты кода или создаёт новый фрагмент
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class Snippet_detail(generics.RetrieveUpdateDestroyAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Получает, обновляет, удаляет фрагмент кода
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerReadOnly]
+
+
+class UserList(generics.ListAPIView):
+    """
+    Выводит всех пользователей или создаёт нового
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    """
+    Получить пользователя
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
