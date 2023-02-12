816294d9fd4f2a36e82f86cd73def84aebb03661
Asus <asus@mail.ru>
2023-02-12 06:35:45 +0300

    Рефакторинг для использования наборов представлений и маршрутизаторов.

--- snippets/views.py
+++ snippets/views.py
@@ -1,64 +1,33 @@
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerReadOnly
-from rest_framework import generics
-from rest_framework import permissions
-from rest_framework.decorators import api_view
+from rest_framework import permissions, renderers, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
-from rest_framework import renderers
 from django.contrib.auth.models import User
 
 
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = [renderers.StaticHTMLRenderer]
-
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
-
-
-class SnippetList(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
     """
-    Выводит все фрагменты кода или создаёт новый фрагмент
-    """
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Получает, обновляет, удаляет фрагмент кода
+    Набор представлений автоматически предоставляет действия 'список',
+    'создать', 'извлечь', 'обновить' и 'уничтожить' + дополнительное действие 'подсветка'.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerReadOnly]
 
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
-class UserList(generics.ListAPIView):
-    """
-    Выводит всех пользователей или создаёт нового
-    """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    Получить пользователя
+    Набор представлений автоматически предоставляет действия 'список', 'создать'
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
